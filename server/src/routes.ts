f707b6c510754cd41fe54bd619dbdcbc922c4074
Leandro Rocha <prog.leandro@gmail.com>
2020-08-09 19:28:26 -0300

    Finalizando a aplicação

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,17 @@
 import express, { response } from "express";
 import ClassesControllers from "./controllers/ClassController";
+import ConnectionControlleer from "./controllers/ConnectionControlleer";
 
 
 const routes = express.Router();
 
 const classControllers = new ClassesControllers();
+const connectionControlleer = new ConnectionControlleer();
 
 routes.get("/classes", classControllers.index);
 routes.post("/classes", classControllers.create);
+
+routes.get("/connections", connectionControlleer.index);
+routes.post("/connections", connectionControlleer.create);
 
 export default routes;
